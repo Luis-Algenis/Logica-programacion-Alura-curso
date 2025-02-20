c8da7c99eea14f1a22c64bd29b9c90772a4cf597
Luis Algenis Florian Alcala <multimarcaslalflal@gmail.com>
2025-02-20 13:57:31 -0400

    represente mis variables con un nombre mas representativo

--- 2035-logica-programacion 2/opcionales/practica 3/1 ejercicio.js
+++ 2035-logica-programacion 2/opcionales/practica 3/1 ejercicio.js
@@ -1,10 +1,10 @@
 //let altura = '1.73 m';
 //let peso = '65kg';
 //IMC = kg/(m)2
-let resultado = "";
-function IMC(altura, peso) {
-   return resultado = peso / (altura * altura);
+let IMC = "";
+function indice(altura, peso) {
+   return IMC = peso / (altura * altura);
 }
 
-IMC(1.73, 65);
-console.log(resultado);
+indice(1.73, 65);
+console.log(IMC);
