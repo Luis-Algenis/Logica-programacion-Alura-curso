6c2c127604e4d3191b8764a99e5b77433464956e
Luis Algenis Florian Alcala <multimarcaslalflal@gmail.com>
2025-02-20 11:59:12 -0400

    realice el primer ejercicio, pero no esta funcionando

---
+++ 2035-logica-programacion 2/opcionales/practica 3/1 ejercicio.js
@@ -0,0 +1,9 @@
+//let altura = '1.73 m';
+//let peso = '65kg';
+//IMC = kg/(m)2
+let resultado = "";
+function IMC(altura, peso) {
+   return resultado = peso / (altura * altura);
+}
+console.log(resultado);
+IMC(1.73, 65);
