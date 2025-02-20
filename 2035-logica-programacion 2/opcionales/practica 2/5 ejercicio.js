ea26bf386873890a691ea9b6f6242e6b67d4c580
Luis Algenis Florian Alcala <multimarcaslalflal@gmail.com>
2025-02-20 11:02:38 -0400

    coloque el archivo que va

---
+++ 2035-logica-programacion 2/opcionales/practica 2/5 ejercicio.js
@@ -0,0 +1,10 @@
+function igualdad(uno, dos) {
+    let mayor;
+    if (uno < dos ) { 
+        console.log(mayor = dos);
+    } else {
+        console.log(mayor = uno);
+    }
+}
+
+igualdad(16,15);
