0d0fa12e917defadda649cfbc9d814575c2e8ee2
Luis Algenis Florian Alcala <multimarcaslalflal@gmail.com>
2025-02-20 11:01:07 -0400

    el archivo anterior no era el correcto

--- 2035-logica-programacion 2/opcionales/practica 2/6 ejercicio.js
+++ 2035-logica-programacion 2/opcionales/practica 2/6 ejercicio.js
@@ -1,6 +1,5 @@
-function duplo(numero) {
-    return 2*numero;
-    }
-    duplo(5);
-    let resultado = 2*numero;
-    console.log(resultado);
+function multiplicacion(numero) {
+    return numero * numero;
+    
+}
+console.log(multiplicacion(5));
