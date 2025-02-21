78e9709fecf01bbc35a2aa25820bb9a738924d15
Luis Algenis Florian Alcala <multimarcaslalflal@gmail.com>
2025-02-21 10:49:13 -0400

    aprendi como hacerlo gracias a chatgpt

---
+++ 2035-logica-programacion 2/opcionales/practica 3/6 ejercicio.js
@@ -0,0 +1,8 @@
+
+function tabla(multiplicar) {
+    for(let multiplo = 0; multiplo <= 12; multiplo++) {
+        console.log(`${multiplicar} x ${multiplo} = ${multiplicar * multiplo}`);
+    }
+}
+
+tabla(2);
