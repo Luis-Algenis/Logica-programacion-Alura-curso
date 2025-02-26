fbdffe3b877bea6c86f605fe8ce627faabe1cb5b
luis algenis florian alcala <multimarcaslalflal@gmail.com>
2025-02-25 20:27:35 -0400

    continuando los ejercicios

--- 2035-logica-programacion 2/opcionales/practica 4/1 ejercicio.js
+++ 2035-logica-programacion 2/opcionales/practica 4/1 ejercicio.js
@@ -1,4 +1,25 @@
-listaGenerica = []
 
-lenguagesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby', 'GoLang']
+let listaGenerica = [];
+//para agregar elemento a mi arreglo debo de anadir lo siguiente: arreglo.push[elemento a arregar].
 
+let lenguagesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby', 'GoLang']
+
+function muestra() {
+    console.log(lenguagesDeProgramacion);
+}
+// Para ver la cantidad de elemento en el arreglo debo de introducir: arreglo.length a el arreglo. 
+// y para ver el ultimo elemento debo de anadir lo siguiente: arreglo[arreglo.length-1].
+muestra();
+
+function reverso() {
+    console.log(lenguagesDeProgramacion.reverse());
+    // agregar llaves [ y dentro posicion del elemento iniciando desde el 0] esto para mostrar un elemento especifico dentro del arreglo.
+}
+
+reverso();
+
+listaGenerica.push(2, 5, 9, 24, 16, 15);
+console.log(listaGenerica);
+function promedio() {
+    
+}
