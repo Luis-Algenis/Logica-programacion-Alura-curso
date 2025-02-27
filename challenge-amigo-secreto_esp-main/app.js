f873ed35755ec303b83680d682e2892a2d290cdd
luis algenis florian alcala <multimarcaslalflal@gmail.com>
2025-02-26 20:47:39 -0400

    comenzando por el amigo secreto

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigoNuevo = [];
+console.log(amigoNuevo);
+let nuevoAmigo = '';
+
+function agregarAmigo() {
+    nuevoAmigo = document.getElementById("amigo");
+    let amigo 
+}
