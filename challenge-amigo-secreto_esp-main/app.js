caae5b310992c9eda439d90124ae7e5631c79174
luis algenis florian alcala <multimarcaslalflal@gmail.com>
2025-02-26 23:33:52 -0400

    cree el array y una funcion que realiza dos tareas

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,9 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+/*Input(id="amigo"), botton(onclick="agregarAmigo()"), ul(id="listaAmigos"), 
+ul(id="resultado"), botton(onclick="sortearAmigo()")
+*/
 let amigoNuevo = [];
-console.log(amigoNuevo);
-let nuevoAmigo = '';
+
 
 function agregarAmigo() {
-    nuevoAmigo = document.getElementById("amigo");
-    let amigo 
+    let nuevoAmigo = document.getElementById("amigo").value;
+    amigoNuevo.push(nuevoAmigo);
+    document.getElementById("listaAmigos").innerHTML = amigoNuevo;
 }
+console.log(amigoNuevo);
+
